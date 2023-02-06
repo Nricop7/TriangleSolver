0b93bd00360ed7a2d35405c9eaa6848eea82f53a
Rico <102043824+Nricop7@users.noreply.github.com>
2023-02-06 13:17:35 -0500

    add isosceles tests

--- TriangleSolver/TriangleSolver.nUnitTesting/TriangleSolverTests.cs
+++ TriangleSolver/TriangleSolver.nUnitTesting/TriangleSolverTests.cs
@@ -25,10 +25,68 @@ namespace TriangleSolver.nUnitTesting
             Assert.AreEqual(expectedResult, actualResult);
         }
 
+
+        //ISOSCELES TESTS
+
+        [Test]
+        public void AnalyzeTriangle_TwoSidesEqual_ValidIsoceles_1()
+        {
+            //Assign
+            int firstSide = 3;
+            int secondSide = 3;
+            int thirdSide = 5;
+            string expectedResult = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+            Triangle triangle = new Triangle();
+
+            //Act
+            string actualResult = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_TwoSidesEqual_ValidIsoceles failed");
+
+        }
+
+        [Test]
+        public void AnalyzeTriangle_TwoSidesEqual_ValidIsoceles_2()
+        {
+            //Assign
+            int firstSide = 2;
+            int secondSide = 4;
+            int thirdSide = 4;
+            string expectedResult = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+            Triangle triangle = new Triangle();
+
+            //Act
+            string actualResult = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_TwoSidesEqual_ValidIsoceles failed");
+
+        }
+
+        [Test]
+        public void AnalyzeTriangle_TwoSidesEqual_ValidIsoceles_3()
+        {
+            //Assign
+            int firstSide = 5;
+            int secondSide = 3;
+            int thirdSide = 5;
+            string expectedResult = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+            Triangle triangle = new Triangle();
+
+            //Act
+            string actualResult = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_TwoSidesEqual_ValidIsoceles failed");
+
+        }
+
+
         // SCALENE TESTS
 
         [Test]
-        public void AnalyzeTriangle_UserInput_ValidScalene_1()
+        public void AnalyzeTriangle_AllSidesDifferent_ValidScalene_1()
         {
             //Assign
             int firstSide = 3;
@@ -41,11 +99,11 @@ namespace TriangleSolver.nUnitTesting
             string actualResult = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
 
             //Assert
-            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_UserInput_ValidScalene failed");
+            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_AllSidesDifferent_ValidScalene failed");
         }
 
         [Test]
-        public void AnalyzeTriangle_UserInput_ValidScalene_2()
+        public void AnalyzeTriangle_AllSidesDifferent_ValidScalene_2()
         {
             //Assign
             int firstSide = 5;
@@ -58,11 +116,11 @@ namespace TriangleSolver.nUnitTesting
             string actualResult = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
 
             //Assert
-            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_UserInput_ValidScalene failed");
+            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_AllSidesDifferent_ValidScalene failed");
         }
 
         [Test]
-        public void AnalyzeTriangle_UserInput_ValidScalene_3()
+        public void AnalyzeTriangle_AllSidesDifferent_ValidScalene_3()
         {
             //Assign
             int firstSide = 5;
@@ -75,11 +133,11 @@ namespace TriangleSolver.nUnitTesting
             string actualResult = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
 
             //Assert
-            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_UserInput_ValidScalene failed");
+            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_AllSidesDifferent_ValidScalene failed");
         }
 
         [Test]
-        public void AnalyzeTriangle_UserInput_ValidScalene_4()
+        public void AnalyzeTriangle_AllSidesDifferent_ValidScalene_4()
         {
             //Assign
             int firstSide = 4;
@@ -92,11 +150,11 @@ namespace TriangleSolver.nUnitTesting
             string actualResult = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
 
             //Assert
-            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_UserInput_ValidScalene_4 failed");
+            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_AllSidesDifferent_ValidScalene failed");
         }
 
         [Test]
-        public void AnalyzeTriangle_UserInput_ValidScalene_5()
+        public void AnalyzeTriangle_AllSidesDifferent_ValidScalene_5()
         {
             //Assign
             int firstSide = 4;
@@ -109,7 +167,7 @@ namespace TriangleSolver.nUnitTesting
             string actualResult = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
 
             //Assert
-            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_UserInput_ValidScalene_4 failed");
+            Assert.AreEqual(expectedResult, actualResult, "AnalyzeTriangle_AllSidesDifferent_ValidScalene failed");
         }
 
 
