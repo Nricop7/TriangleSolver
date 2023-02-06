42eed1b0ef2dc82f3ab8ad08b8647fddfe494e0f
Rico <102043824+Nricop7@users.noreply.github.com>
2023-02-06 12:05:15 -0500

    add nUnitTesting

---
+++ TriangleSolver/TriangleSolver.nUnitTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TriangleSolver.nUnitTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
